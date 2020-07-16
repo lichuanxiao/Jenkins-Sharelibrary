32d13f1d6d1ce39ff8fc158a93fe255533e83973
Hayden <45088633+ghl1024@users.noreply.github.com>
2020-07-16 17:45:26 +0800

    Create downloadTheWar_common.groovy

---
+++ src/org/devops/downloadTheWar_common.groovy
@@ -0,0 +1,21 @@
+def  call(String servicename='',String serviceNameBatchDeployServer='',String patternName='',String warName='',String download_path=''){ 
+	if(serviceNameBatchDeployServer.length() != 0 ){
+        //从制品库下载包
+        sh "curl --user '${nexusRawUsernamePassword}'  --o  ${env.WarName}  http://119.3.223.138:8081/repository/raw-devops/devops/${params.nexusRawRepository}"
+        //下载脚本
+        sh "/usr/bin/git clone git@github.com:ghl1024/Jenkinsfile.git"	                         
+        def serverlist=serviceNameBatchDeployServer.split('\\|')
+        for(values in serverlist){
+            //values=serverlist[0]
+            list1=values.split(',') 
+            print(list1[0]+" "+list1[1]+" "+list1[2])						  
+            listServerIP=list1[0]
+            listServerPort=list1[1]
+            listServerUser=list1[2]	
+            //批量拷贝
+            sh "ansible ${env.serverIP} -u ${env.serverName} -m  shell -a  'mkdir -p /home/${env.serverName}/tmp'"
+            sh "ansible ${env.serverIP} -u ${env.serverName} -m  copy -a  'src=${WORKSPACE}/Jenkinsfile/jenkins dest=/home/${env.serverName} mode=0755'"
+            sh "ansible ${env.serverIP} -u ${env.serverName} -m  copy -a  'src=${WORKSPACE}/${env.WarName} dest=/home/${env.serverName}/tmp/${env.WarName} mode=0755'"  
+        }	
+    }						        
+}

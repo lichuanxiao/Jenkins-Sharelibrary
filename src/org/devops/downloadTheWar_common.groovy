651baa09f95474f78d3ca0f0c8c1991eede1cfe8
Hayden <45088633+ghl1024@users.noreply.github.com>
2020-07-16 18:42:55 +0800

    Update downloadTheWar_common.groovy

--- src/org/devops/downloadTheWar_common.groovy
+++ src/org/devops/downloadTheWar_common.groovy
@@ -1,13 +1,13 @@
-def  call(String servicename='',String BatchDeployServer='',String warName=''){ 
+def  call(String servicename='',String BatchDeployServer='',String WarName=''){ 
 	if(BatchDeployServer.length() != 0 ){
         //从制品库下载包
-        sh "curl --user '${nexusRawUsernamePassword}'  --o  ${env.WarName}  http://119.3.223.138:8081/repository/raw-devops/devops/${params.nexusRawRepository}"
+        sh "curl --user '${nexusRawUsernamePassword}'  --o  ${WarName}  http://119.3.223.138:8081/repository/raw-devops/devops/${params.nexusRawRepository}"
         //下载脚本
         sh "/usr/bin/git clone git@github.com:ghl1024/Jenkinsfile.git"	                         
-        def serverlist=serviceNameBatchDeployServer.split('\\|')
+        def serverlist=BatchDeployServer.split('\\|')
         for(values in serverlist){
-			//values=serverlist[0]
-			list1=values.split(',') 
+            //values=serverlist[0]
+            list1=values.split(',') 
             print(list1[0]+" "+list1[1]+" "+list1[2])						  
             listServerIP=list1[0]
             listServerPort=list1[1]

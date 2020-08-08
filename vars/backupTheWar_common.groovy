201e3fcf04a6fa0d07a7d2742e307ff57b084ac9
Hayden <45088633+ghl1024@users.noreply.github.com>
2020-08-08 18:34:08 +0800

    Update backupTheWar_common.groovy

--- vars/backupTheWar_common.groovy
+++ vars/backupTheWar_common.groovy
@@ -8,7 +8,7 @@ def call(String yanzheng='',String servicename='',String batchDeployServer='',St
             listServerIP=list1[0]
             listServerPort=list1[1]
             listServerUser=list1[2]
-            sh "ansible  ${listServerIP}  -u ${listServerUser} -m copy -a 'src=${WORKSPACE}/devops/JenkinsCICD/jenkins dest=/home/${listServerUser} mode=0755'"
+            sh "ansible  ${listServerIP}  -u ${listServerUser} -m copy -a 'src=${workspace}/${env.servicename}/JenkinsCICD/jenkins dest=/home/${listServerUser} mode=0755'"
             sh "ansible  ${listServerIP}  -u ${listServerUser} -m shell -a  'sh ${bsn}'"
         }
         return " ${batchDeployServer} backup is comeplete"	        

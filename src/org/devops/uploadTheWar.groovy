e4797615394a58df4e7208cbe954fb858180e2cd
Hayden <45088633+ghl1024@users.noreply.github.com>
2020-08-08 15:54:33 +0800

    Update uploadTheWar.groovy

--- src/org/devops/uploadTheWar.groovy
+++ src/org/devops/uploadTheWar.groovy
@@ -1,4 +1,4 @@
-echo "test"
+//echo "test"
 /**
 def call(String warName='',String targetName=''){ 
     rtServer (
@@ -16,3 +16,19 @@ def call(String warName='',String targetName=''){
     archiveArtifacts artifacts: "${warName}", fingerprint: true					
  }
 **/
+def  call(String warName='',String targetName=''){ 
+		      rtServer (
+                    id: "ARTIFACTORY_SERVER",
+                    url: "http://172.16.136.13:8081/artifactory",
+                    credentialsId: "artifactory-admin",
+                )
+               rtUpload(
+				   serverId: "ARTIFACTORY_SERVER",
+				   spec:"""{"files":[{"pattern":"${warName}","target":"${targetName}"}]}"""
+				)
+				rtPublishBuildInfo (
+                    serverId: "ARTIFACTORY_SERVER"
+                )			
+               archiveArtifacts artifacts: "${warName}", fingerprint: true					
+       
+}
